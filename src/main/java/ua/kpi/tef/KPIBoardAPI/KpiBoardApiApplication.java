43637955b269ef833161d185803199c1718a9a1a
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2021-03-13 18:25:30 +0200

    Initial commit

---
+++ src/main/java/ua/kpi/tef/KPIBoardAPI/KpiBoardApiApplication.java
@@ -0,0 +1,13 @@
+package ua.kpi.tef.KPIBoardAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpiBoardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpiBoardApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/kpi/tef/KPIBoardAPI/KpiBoardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kpi.tef.KPIBoardAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpiBoardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
